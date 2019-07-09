40f5da4238b778e7672075c6a36c5785d9ead54a
xuanzi820 <youngclyang@163.com>
2019-07-09 23:19:39 +0800

    封装api请求函数

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -28,7 +28,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+import { login } from '@/api/user'
 export default {
   name: 'LoginIndex',
   data () {
@@ -41,12 +41,13 @@ export default {
   },
   methods: {
     async handleLogin () {
-      const res = await axios({
-        method: 'POST',
-        url: 'http://toutiao.course.itcast.cn/app/v1_0/authorizations',
-        data: this.user
-      })
-      console.log(res)
+      try {
+        const data = await login(this.user)
+        console.log(data)
+      } catch (err) {
+        console.log(err)
+        console.log('登录失败')
+      }
     }
   }
 }
