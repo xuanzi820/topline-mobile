81afde837bcc7839094bdff0a0abed2b711bc6b7
xuanzi820 <youngclyang@163.com>
2019-07-09 09:11:23 +0800

    准备两个页面组件

---
+++ src/views/home/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Home component</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIndex'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Login component</div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
