7dc8ae4c8d3658878b0c1fabe21c2bd1b34f7fa8
xuanzi820 <youngclyang@163.com>
2019-07-09 22:50:47 +0800

    基于axios封装request请求模块

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Home component</div>
+  <div>
+    <div class="box"></div>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <script>
@@ -9,4 +16,9 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.box {
+  width: 375px;
+  height: 200px;
+  background-color: #f40;
+}
 </style>
