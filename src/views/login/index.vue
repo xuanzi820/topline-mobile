6fb271c32e82fcdcde2bffe42f36defd0408c88b
xuanzi820 <youngclyang@163.com>
2019-07-11 11:00:33 +0800

    配置登录表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,17 +7,22 @@
       <van-cell-group>
       <van-field
         v-model="user.mobile"
+        v-validate="'required'"
+        name="mobile"
+        :error-message="errors.first('mobile')"
         required
         clearable
         label="手机号"
         placeholder="请输入手机号"
-        :error-message="mobileMessage"
       />
       <van-field
         v-model="user.code"
-        type="password"
-        label="密码"
-        placeholder="请输入密码"
+        v-validate="'required'"
+        name="code"
+        :error-message="errors.first('code')"
+        type="code"
+        label="验证码"
+        placeholder="请输入验证码"
         required
       />
       </van-cell-group>
@@ -37,9 +42,11 @@ export default {
       user: {
         mobile: '15733298674',
         code: '123456'
-      },
-      mobileMessage: ''
+      }
     }
+  },
+  created () {
+    this.configCustomMessages()
   },
   methods: {
     async handleLogin () {
@@ -50,14 +57,33 @@ export default {
       //   return
       // }
       try {
-        const data = await login(this.user)
-        console.log(data)
-        // 通过提交mutation更新vuex容器中的状态
-        this.$store.commit('setUser', data)
+        this.$validator.validate().then(async valid => {
+          // 如果表单验证失败，则什么都不做
+          if (!valid) {
+            return
+          }
+          const data = await login(this.user)
+          console.log(data)
+          // 通过提交mutation更新vuex容器中的状态
+          this.$store.commit('setUser', data)
+        })
       } catch (err) {
         console.log(err)
         console.log('登录失败')
       }
+    },
+    configCustomMessages () {
+      const dict = {
+        custom: {
+          mobile: {
+            required: '手机号不能为空'
+          },
+          code: {
+            required: () => '验证码不能为空'
+          }
+        }
+      }
+      this.$validator.localize('zh_CN', dict)
     }
   }
 }
