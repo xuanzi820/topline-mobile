780bb445e6bffdd2ee618ec11facd4b3fce5eea2
xuanzi820 <youngclyang@163.com>
2019-07-17 00:04:16 +0800

    文章列表-配置dayjs日期处理库

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import 'amfe-flexible'
 import './styles/index.less'
 import zhCN from 'vee-validate/dist/locale/zh_CN'
 import VeeValidate, { Validator } from 'vee-validate'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn' // 加载中文语言包
+import relativeTime from 'dayjs/plugin/relativeTime' // dayjs 的 相对时间插件
+dayjs.extend(relativeTime) // 把插件注册到 dayjs 中
+dayjs.locale('zh-cn') // 配置使用中文语言包
 Vue.use(VeeValidate, {
   // 配置改变的时候去触发校验，默认是input
   // 配置文档：https://baianat.github.io/vee-validate/guide/events.html#changing-default-events
